22a5847ef1296639002b3fea869ba3e6a25c8e16
sarthakjana09 <sarthakjana09@gmail.com>
2025-02-11 22:33:49 +0530

    Anonymous runnable

--- src/concurrency/Application.java
+++ src/concurrency/Application.java
@@ -4,8 +4,25 @@ public class Application {
 
     public static void main(String[] args) {
         MyThread myThread = new MyThread();
-        myThread.run();
-        MyThread myThread1 = new MyThread();
-        myThread1.run();
+        //myThread.start();
+
+
+        RunnableThread runnableThread = new RunnableThread();
+
+        Thread thread = new Thread(runnableThread);
+//        thread.start();
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i <= 5; i++) {
+                System.out.println(Thread.currentThread().getName() + " " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread1.start();
+
     }
 }
